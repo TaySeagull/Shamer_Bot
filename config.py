6fa54e0466a3f9fb7ad69e54b7df938b9bdb81b6
TaySeagull <tvzhar457@gmail.com>
2024-04-22 22:37:58 +0300

    первые шаги + переделано использование settings

---
+++ config.py
@@ -0,0 +1,6 @@
+settings = {
+    'token': 'MTIzMjAyNTA0NzAwMzMwNDAyOA.GFoUdG.SYR7eHQFw-bnzQfplo1hSkmysXDh92hd3OxWlo',
+    'bot': 'shamer_bot',
+    'id': 1232025047003304028,
+    'prefix': '$'
+}
--- main.py
+++ main.py
@@ -1,15 +1,46 @@
-from utils import load_config
+#from utils import load_config
+from config import settings
 import discord
 from discord.ext import commands
+import logging
 
 
-SETTINGS = load_config('config.ini')
-TOKEN = SETTINGS['TOKEN']
-DB_PATH = SETTINGS['DB_PATH']
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+#SETTINGS = load_config('config.ini')
+#TOKEN = SETTINGS['TOKEN']
+#DB_PATH = SETTINGS['DB_PATH']
+
+
+class YLBotClient(discord.Client):
+    async def on_ready(self):
+        logger.info(f'{self.user} has connected to Discord!')
+        for guild in self.guilds:
+            logger.info(
+                f'{self.user} подключились к чату:\n'
+                f'{guild.name}(id: {guild.id})')
+
+    async def on_member_join(self, member):
+        await member.create_dm()
+        await member.dm_channel.send(
+            f'Привет, {member.name}!'
+        )
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if "привет" in message.content.lower():
+            await message.channel.send("И тебе привет")
+        if "я позорище" in message.content.lower():
+            await message.channel.send("Ну-ну, все мы не без греха")
 
 
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix='$', intents=intents, help_command=None)
-
-
-
+intents.members = True
+intents.message_content = True
+client = YLBotClient(intents=intents)
+client.run(settings["token"])
